5b7bb36d02bb3fc994c3929d65666ef3c9e92567
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-07-11 11:56:02 -0300

    update

--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,11 @@
-
-
-import { z } from 'zod'
+import { z } from "zod";
 
 const envSchema = z.object({
-    DATABASE_URL: z.string().url(),
-    API_BASE_URL: z.string().url(),
-    WEB_BASE_URL: z.string().url(),
-    
-    PORT: z.coerce.number().default(3333)
-})
+  DATABASE_URL: z.string().url(),
+  API_BASE_URL: z.string().url(),
+  WEB_BASE_URL: z.string().url(),
 
+  PORT: z.coerce.number().default(3333),
+});
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env);
